d710299594ab18181703cc8f40c7b3144443b525
Muhammed Oladapo <muhammedoladapo3@gmail.com>
2022-08-07 20:54:27 +0100

    adding urls and views

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,7 @@
 from urllib import request
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import Http404
+from markdown2 import Markdown
 from . import util
 
 
@@ -8,3 +9,12 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def wiki(request, entry):
+    if entry not in util.list_entries():
+        raise Http404
+    content = util.get_entry(entry)
+    return render(request, "encyclopedia/wiki.html",
+        {"title": entry, "content": Markdown().content(content)}
+    )
+    
