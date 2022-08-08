8e25452e953546b89e6d4570fd5868f85156f57b
Muhammed Oladapo <muhammedoladapo3@gmail.com>
2022-08-08 17:21:51 +0100

    upgraded the index html page

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,8 +5,13 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>", views.wiki, name="wiki"),
-    path("search/", views.search, name="search")
+    path("search/", views.search, name="search"),
+    path("new", views.new, name="new"),
+    path("random", views.random_entry, name="random_entry"),
+    path("edit/<str:entry>", views.edit, name="edit")
 ]
+
+handler404 = "encyclopedia.views.handler404"
 
 
 
